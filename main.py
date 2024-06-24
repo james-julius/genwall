ff4a6516f3cc949165ac4e61b30dc5e858ac8cc1
james-julius <jamesdarby7@gmail.com>
2024-06-23 22:40:58 -0700

    feat: setup weather api call and image generation call

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import requests
+import geocoder
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
+
+def main():
+    g = geocoder.ip('me')
+    current_weather = get_current_weather_prompt(g.latlng)
+    background_file = make_stable_diffusion_background("An incredible desktop wallpaper of San Francisco at night, lofi hacker looking out of window")
+    filename = datetime.now().strftime("%d-%m-%Y | %H:%M:S")
+    with open(f"./wallpapers/{filename}", 'wb') as file:
+        file.write(background_file)
+
+def get_current_weather_prompt(latlng):
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": latlng[0],
+        "longitude": latlng[1],
+        "current": "temperature_2m,wind_speed_10m",
+        "aspect_ratio": "16:9",
+        # "output_format": "jpeg"
+    }
+
+    response = requests.get(url, params=params)
+
+    # Print the response in JSON format
+    print(response.json())
+
+
+def make_stable_diffusion_background(prompt):
+    api_key = os.getenv("STABILITY_AI_API_KEY")
+    response = requests.post(
+            f"https://api.stability.ai/v2beta/stable-image/generate/ultra",
+            headers={
+                "authorization": f"Bearer {api_key}",
+                "accept": "image/*"
+            },
+            files={"none": ''},
+            data={
+                "prompt": prompt,
+                "output_format": "webp",
+            },
+        )
+
+    if response.status_code == 200:
+        return response.content
+    else:
+        raise Exception(str(response.json()))
+main()
